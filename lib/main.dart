bc290b7f7401876e97b053bb410d2e208092ca94
Mohammad Hasib <hasibhabibzadah786@gmail.com>
2023-06-07 15:21:06 +0430

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,66 @@
+// Import the packages
 import 'package:flutter/material.dart';
+import 'dart:math';
 
+// Running the main core of project using responsive design
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue,
         appBar: AppBar(
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.blue,
           title: Text('Dice'),
         ),
-        body: MyApp(),
+        body: const Dice(),
       ),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class Dice extends StatefulWidget {
+  const Dice({super.key});
+
+  @override
+  State<Dice> createState() => _DiceState();
+}
+
+
+// Generate rnadom Integer numbers between 0 and 6
+class _DiceState extends State<Dice> {
+  int leftDice = 1;
+  int rightDice = 1;
+  void changeDices() {
+    leftDice = Random().nextInt(6) + 1;
+    rightDice = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: TextButton(
+                child: Image(image: AssetImage('images/dice$leftDice.png')),
+                onPressed: () {
+                  setState(() {
+                    changeDices();
+                  });
+                },
+              )),
+          Expanded(
+              child: TextButton(
+                child: Image(image: AssetImage('images/dice$rightDice.png')),
+                onPressed: () {
+                  setState(() {
+                    changeDices();
+                  });
+                },
+              )),
+        ],
+      ),
+    );
   }
 }
